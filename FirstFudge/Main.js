2fe8a62ff2d815cd31e555c5b3ecbf73ae4aa3fc
ThyraHultsch <64130749+ThyraHultsch@users.noreply.github.com>
2020-10-14 16:06:53 +0200

    test2

---
+++ FirstFudge/Main.js
@@ -0,0 +1,8 @@
+"use strict";
+var L01FirstFudge;
+(function (L01FirstFudge) {
+    console.log("Hallo");
+    var fc = FudgeCore;
+    console.log(fc);
+})(L01FirstFudge || (L01FirstFudge = {}));
+//# sourceMappingURL=Main.js.map
---
+++ FirstFudge/Main.ts
@@ -0,0 +1,9 @@
+namespace L01FirstFudge {
+
+    console.log("Hallo");
+
+    import fc = FudgeCore;
+    
+    console.log(fc);
+
+}
